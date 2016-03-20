1a961ad7997d3830049d54335ace6222469f316e
Andrew D'Angelo <dangeloandrew@outlook.com>
2016-03-20 17:21:59 -0500

    Add prelude.s

---
+++ prelude.s
@@ -0,0 +1,19 @@
+	.cpu arm7tdmi
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.arm
+	.syntax divided
+	
+	.text
+	.align	2
+	.global	scheme_entry
+	.type	scheme_entry, %function
+scheme_entry:
